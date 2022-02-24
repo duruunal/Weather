7498136d260e005189c13c92433db1fa719b579c
duruunal <dunal@tfs.ca>
2022-02-24 06:53:17 -0500

    final

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,23 +1,37 @@
 package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     //decraration des champs
-    TextView mDate,mCity,mTemp,mDescription;
+    TextView mDate,mCity,mTemp,mDescription,mHumidity,mPressure;
     ImageView imgIcon;
     String maVille="Toronto";
 
@@ -30,11 +44,45 @@ public class MainActivity extends AppCompatActivity {
         mCity=findViewById(R.id.mCity);
         mTemp=findViewById(R.id.mTemp);
         mDescription=findViewById(R.id.mDescription);
+        mHumidity=findViewById(R.id.mHumidity);
+        mPressure=findViewById(R.id.mPressure);
+        afficher(); //appel de la methode
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.recherche, menu);
+        MenuItem menuItem=menu.findItem(R.id.action_search);
+        SearchView searchView=(SearchView)menuItem.getActionView();
+        searchView.setQueryHint("Ecrire le nom de la ville");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                maVille=query;
+                afficher();
+                //gestion du clavrier
+                InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                if(getCurrentFocus() !=null)
+                {
+                    inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     public void afficher()
     {
-        String url="https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=f260cabba1344937ee405442fd78638b&units=metric";
+        String url="https://api.openweathermap.org/data/2.5/weather?q="+ maVille + "&appid=f260cabba1344937ee405442fd78638b&units=metric";
         JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
             @Override
@@ -43,7 +91,35 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     JSONObject main_object=response.getJSONObject("main");
                     JSONArray array = response.getJSONArray("weather");
-                    Log.d("Tag", "resultat = "+array.toString());
+                   // Log.d("Tag", "resultat = "+array.toString());
+                    JSONObject object = array.getJSONObject(0);
+                    //temperature
+                    int tempC=(int)Math.round(main_object.getDouble("temp"));
+                    String temp=String.valueOf(tempC);
+
+                    String description=object.getString("description");
+                    String city=response.getString("city");
+                    String icon= object.getString("icon");
+                    String humidity=response.getString("humidity");
+                    String pressure=response.getString("pressure");
+                    //mettre les valeurs dans les champs
+                    mCity.setText(city);
+                    mTemp.setText(temp);
+                    mDescription.setText(description);
+                    mHumidity.setText(humidity);
+                    mPressure.setText(pressure);
+                    //formattage du temps
+                    Calendar calendar=Calendar.getInstance();
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE, MMMM dd");
+                    String formatted_date= simpleDateFormat.format(calendar.getTime());
+
+                    mDate.setText(formatted_date);
+                    //gestion de l'image
+                    String imageUri="http://openweathermap.org/img/w/"+ icon+ ".png";
+                    imgIcon=findViewById(R.id.imgicon);
+                    Uri myUri=Uri.parse(imageUri);
+                    Picasso.with(MainActivity.this).load(myUri).resize(200, 200).into(imgIcon);
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -54,5 +130,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        //ajouter l'object dans la pile d'attente
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjectRequest);
     }
 }
